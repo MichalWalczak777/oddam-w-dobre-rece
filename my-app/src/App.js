ea8d9292a54116884e51b956b05fe38005ea7bf1
Michal Walczak <michalwalczak777@gmail.com>
2020-12-23 02:20:18 +0100

    login component with routing has been created and styled + structure fix

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,7 @@
 import './scss/main.scss';
 import Home from './components/Home';
+import HomeHeader from "./components/HomeHeader";
+import Login from './components/Login';
 import {
   HashRouter,
   Route,
@@ -9,8 +11,10 @@ import {
 function App() {
   return (
     <HashRouter>
+      <HomeHeader/>
       <Switch>
-          <Route exact path="/" component={Home}/>                                                      
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/login" component={Login}/>                                                     
       </Switch>
     </HashRouter>
   );
