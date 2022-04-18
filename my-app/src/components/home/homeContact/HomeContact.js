6745d33c3cb0f71b48e5c431da62485d1a4a802f
Michal Walczak <michalwalczak777@gmail.com>
2022-04-18 14:05:09 +0200

    Fix styling of HomeContact component

--- my-app/src/components/home/homeContact/HomeContact.js
+++ my-app/src/components/home/homeContact/HomeContact.js
@@ -1,8 +1,6 @@
 import React from "react";
-import {useState, useRef} from "react";
+import {useState} from "react";
 import decoration from "../../../assets/Decoration.svg";
-import facebook from "../../../assets/Facebook.svg";
-import instagram from "../../../assets/Instagram.svg";
 import axios from "axios";
 
 const HomeContact = () => {
@@ -22,9 +20,6 @@ const HomeContact = () => {
             message: []
     });
 
-    const nameErrors = useRef(null);
-    const emailErrors = useRef(null);
-    const messageErrors = useRef(null);
 
     const axiosHeader = {
         "Content-Type": "application/json"
@@ -89,34 +84,29 @@ const HomeContact = () => {
                         <div className="homeContact-formBox">
                             <div className="homeContact-inputBox">
                                 <label htmlFor="name">Wpisz swoje imię</label>
-                                <input type="text" name="name" id="name" placeholder="Krzysztof" onChange={handleChange}/>
+                                <div className='homeContact-inputWrapper'>
+                                    <input type="text" name="name" id="name" placeholder="Krzysztof" onChange={handleChange}/>
+                                </div>
                                 <div className="homeContact-errors">{errors.name}</div>
                             </div>
                             <div className="homeContact-inputBox">
                                 <label htmlFor="email">Wpisz swój email</label>
-                                <input type="email" name="email" id="email" placeholder="abc@xyz.pl" onChange={handleChange}/>
+                                <div className='homeContact-inputWrapper'>
+                                    <input type="email" name="email" id="email" placeholder="abc@xyz.pl" onChange={handleChange}/>
+                                </div>
                                 <div className="homeContact-errors"> {errors.email}</div>
                             </div>
                         </div>
                         <div className="homeContact-inputBox homeContact-messageBox">
                             <label htmlFor="message">Wpisz swoją wiadomość</label>
-                            <textarea name="message" id="message" onChange={handleChange} 
-                                      placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
+                                <textarea name="message" id="message" onChange={handleChange} 
+                                        placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation."/>
                             <div className="homeContact-errors">{errors.message}  </div>
                         </div>
                         <div className="homeContact-buttonWrapper">
                             <button className="homeContact-submit" type="submit">Wyślij</button>
                         </div>
                     </form>
-                </div>
-                <div className="homeContact-footer">
-                    <div></div>
-                    <p>Copyright by Coderslab</p>
-                    <div className="homeContact-footerIcons">
-                        <img alt="facebookIcon" src={facebook}></img>
-                        <img alt="instagramIcon" src={instagram}></img>
-                    </div>
                 </div>
             </div>  
         </div>
