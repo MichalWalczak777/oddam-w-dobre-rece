a96d507eafc2c78c2be825022cfb9b453c4401b9
Michal Walczak <michalwalczak777@gmail.com>
2022-05-27 23:56:50 +0200

    Refactor validateFields function in DonateStuff component for a cleaner code + export regular expressions to a separate file

--- my-app/src/components/donateStuff/DonateStuff.js
+++ my-app/src/components/donateStuff/DonateStuff.js
@@ -8,11 +8,11 @@ import DonateStep3 from "./DonateStep3";
 import DonateStep4 from "./DonateStep4";
 import DonateStep5 from "./DonateStep5";
 import DonateStep6 from "./DonateStep6";
+import { postalCodeRegex, phoneNumberRegex, dateRegex, hourRegex } from "./donateFormRegex";
 
 const DonateStuff = () => {
 
     const [step, setStep] = useState(1);
-
     const [fields, setFields] = useState({
         itemsToDonate: [],
         numberOfBags: "",
@@ -27,7 +27,6 @@ const DonateStuff = () => {
         hour: "",
         message: ""
     })
-
     const [errors, setErrors] = useState({
         itemsToDonate: "",
         numberOfBags: "",
@@ -48,30 +47,55 @@ const DonateStuff = () => {
         console.log("wysłano");
         nextStep();
     }
-
     const handleChange = e =>{
         const {name, value} = e.target;
         setFields(prev => ({
             ...prev,
             [name] : value}));
     }
-
     const handleChangeCheckbox = e => {
-            const {name, value} = e.target;
-            fields[name]?.includes(value)
-            ?
-            setFields(prev => ({
-                ...prev,
-                [name]: [...prev[name]].filter(e => e !== value)}))
-            :
-            setFields(prev => ({
-                ...prev,
-                [name] : [...prev[name], value]}))
+        const {name, value} = e.target;
+        fields[name]?.includes(value)
+        ?
+        setFields(prev => ({
+            ...prev,
+            [name]: [...prev[name]].filter(e => e !== value)}))
+        :
+        setFields(prev => ({
+            ...prev,
+            [name] : [...prev[name], value]}))
     }
 
+
+    const findPage1Errors = (errorsFound) => {
+
+        fields.itemsToDonate.length === 0 && (errorsFound.itemsToDonate = "musisz wybrać przedmioty do oddania");
+        return errorsFound;
+    }
+    const findPage2Errors = (errorsFound) => {
+
+        !fields.numberOfBags && (errorsFound.numberOfBags = "musisz wybrać liczbę worków");
+        return errorsFound;
+    }
+    const findPage3Errors = (errorsFound) => {
+        
+        !fields.location && (errorsFound.location = "musisz wybrać miasto");
+        fields.beneficiaries.length === 0 && !fields.organization ? errorsFound.beneficiaries = ("musisz wybrać komu chcesz pomóc") : errorsFound.beneficiaries = "";
+        return errorsFound;
+    }
+    const findPage4Errors = (errorsFound) => {
+
+        fields.street.length < 2 && (errorsFound.street = "musisz wpisać nazwę ulicy");
+        fields.city.length < 2 && (errorsFound.city = "musisz wpisać nazwę miasta");
+        !postalCodeRegex.test(fields.postalCode) ? errorsFound.location = "kod pocztowy w formacie xx-xxx": errorsFound.location = "";
+        !phoneNumberRegex.test(fields.phoneNumber) ? errorsFound.phoneNumber = "podaj swój numer telefonu": errorsFound.phoneNumber = "";
+        !dateRegex.test(fields.date) ? errorsFound.date = "musisz wybrać datę w formacie dd/mm/rr": errorsFound.date = "";
+        !hourRegex.test(fields.hour) ? errorsFound.hour = "wpisz godzinę w formacie gg:mm": errorsFound.hour = "";
+        return errorsFound;
+    }
     const validateFields = page =>{
 
-        const errorsFound = {
+        let errorsFound = {
             itemsToDonate: "",
             numberOfBags: "",
             location: "",
@@ -85,54 +109,27 @@ const DonateStuff = () => {
             hour: "",
             message: ""
         }
-
-        if (page === 1){
-            fields.itemsToDonate.length === 0 && (errorsFound.itemsToDonate = "musisz wybrać przedmioty do oddania");
-            setErrors(errorsFound);
-            return (!errorsFound.itemsToDonate);
+        switch (page){
+            case 1:
+                errorsFound = findPage1Errors(errorsFound);
+                break;
+            case 2:
+                errorsFound = findPage2Errors(errorsFound);
+                break;
+            case 3:
+                errorsFound = findPage3Errors(errorsFound);
+                break;            
+            case 4:
+                errorsFound = findPage4Errors(errorsFound);
+                break;
         }
-        else if (page === 2){
-            !fields.numberOfBags && (errorsFound.numberOfBags = "musisz wybrać liczbę worków");
-            setErrors(errorsFound);
-            return (!errorsFound.numberOfBags);
-        }
-        else if (page === 3){
-            !fields.location && (errorsFound.location = "musisz wybrać miasto");
-            fields.beneficiaries.length === 0 && !fields.organization ? errorsFound.beneficiaries = ("musisz wybrać komu chcesz pomóc") : errorsFound.beneficiaries = "";
-            
-            setErrors(errorsFound);
-            return (!errorsFound.location && !errorsFound.beneficiaries);
-        }
-        else if (page === 4){
-            fields.street.length < 2 && (errorsFound.street = "musisz wpisać nazwę ulicy");
-            fields.city.length < 2 && (errorsFound.city = "musisz wpisać nazwę miasta");
-
-            const postalCodeRegex = /d{2}-\d{3}/;
-            !postalCodeRegex.test(fields.postalCode) ? errorsFound.location = "kod pocztowy w formacie xx-xxx": errorsFound.location = "";
-
-            const phoneNumberRegex = /^\d{9}$/;
-            !phoneNumberRegex.test(fields.phoneNumber) ? errorsFound.phoneNumber = "podaj swój numer telefonu": errorsFound.phoneNumber = "";
-
-            const dateRegex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
-            !dateRegex.test(fields.date) ? errorsFound.date = "musisz wybrać datę w formacie dd/mm/rr": errorsFound.date = "";
-
-            const hourRegex = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/;
-            !hourRegex.test(fields.hour) ? errorsFound.hour = "wpisz godzinę w formacie gg:mm": errorsFound.hour = "";
-
-            setErrors(errorsFound);
-            return(!errorsFound.street && !errorsFound.city && !errorsFound.postalCode
-                 && !errorsFound.phoneNumber && !errorsFound.date && !errorsFound.hour)
-        }
-        else{
-            return true;
-        }
-
+        setErrors(errorsFound);
+        const isCorrect = Object.values(errorsFound).every(errorField => errorField === "");
+        return isCorrect;
     }
-
     const previousStep = () => {
         setStep(prev => prev-1);
     }
-
     const nextStep = () => {
         validateFields(step) && setStep(prev => prev+1);
     }
